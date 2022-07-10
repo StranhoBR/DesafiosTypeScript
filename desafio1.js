3f32a11521b736e5c7cde6125e3ae0f8b7626d32
StranhoBR <stranhol2@gmail.com>
2022-07-10 20:30:42 -0300

    desafio2

--- desafio1.js
+++
@@ -1,5 +0,0 @@
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
-var employee = {
-    code: 10,
-    name: "John"
-};
---
+++ desafio2.ts
@@ -0,0 +1,34 @@
+// Como podemos melhorar o esse código usando TS?
+
+enum Trabalho {
+    Atriz,
+    Padeiro
+}
+
+type Humano = {nome: String, idade: Number, profissao: Trabalho}
+
+let pessoa1: Humano = {
+    nome: "maria",
+    idade: 29,
+    profissao: Trabalho.Atriz
+};
+
+
+let pessoa2: Humano = {
+    nome: "roberto",
+    idade: 19,
+    profissao: Trabalho.Padeiro
+}
+
+
+let pessoa3: Humano = {
+    nome: "laura",
+    idade: 32,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa4: Humano = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Trabalho.Padeiro
+}
